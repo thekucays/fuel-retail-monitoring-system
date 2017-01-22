0b805b562ec0b85b6a1c284e970998d121bc17e2
Luki R Daniel <luki@ptdam.com>
2017-01-22 14:20:59 +0700

    dressing up #1 - login page

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <html>
 	<head>
 		<title> Fuel Retail v0.1 Login Page</title>
+		<link rel="stylesheet" href="css\loginpage.css"
 	</head>
 	<body>
-		Silahkan Login
-		<form name="loginform" method="post" id="loginform" action="classes\controller\LoginController.php">
-			<table>
-				<tr>
-					<td> NIP </td>
-					<td> <input type="text" name="nip" id="nip" /> </td>
-				</tr>
-				<tr>
-					<td> Password </td>
-					<td> <input type="password" name="pass" id="pass" /> </td>
-				</tr>
-				<tr>
-					<td colspan=2>
-						<input type="submit" value="login" />
-					</td>
-				</tr>
-			</table>
-		</form>
+		<div class="container">
+		<div class="card card-container">
+			<img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+            <p id="profile-name" class="profile-name-card"></p>
+			
+			<form class="form-signin" name="loginform" method="post" id="loginform" action="classes\controller\LoginController.php">
+				<input type="text" id="nip" name="nip" class="form-control" placeholder="Email address" required autofocus>
+                <input type="password" id="pass" name="pass" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+			</form>
+		</div>
+		</div>
 	</body>
 </html>
